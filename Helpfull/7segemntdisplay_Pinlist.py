ba2d3c1d4c7b2174c0d34a28effd7ae8200667fe
Max <161768135+Picoodi@users.noreply.github.com>
2024-03-05 22:43:54 +0100

    Update 7segemntdisplay_Pinlist.py

--- Helpfull/7segemntdisplay_Pinlist.py
+++ Helpfull/7segemntdisplay_Pinlist.py
@@ -1,10 +1,16 @@
 from machine import Pin
 
+
+d1 = machine.Pin((); machien.Pin.OUT)
+d2 = machine.Pin((); machien.Pin.OUT)
+d3 = machine.Pin((); machien.Pin.OUT)
+d4 = machine.Pin((); machien.Pin.OUT)
+
 a = machine.Pin((), machine.Pin.OUT)
 b = machine.Pin((), machine.Pin.OUT)
 c = machine.Pin((), machine.Pin.OUT)
 d = machine.Pin((), machine.Pin.OUT)
 e = machine.Pin((), machine.Pin.OUT)
 f = machine.Pin((), machine.Pin.OUT)
-bottom = machine.Pin((), machine.Pin.OUT)
-bottomleft = machine.Pin((), machine.Pin.OUT)
+g = machine.Pin((), machine.Pin.OUT)
+dp = machine.Pin((), machine.Pin.OUT)
