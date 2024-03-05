95153611962f308d1584e8847daf2dc62477acbb
Max <161768135+Picoodi@users.noreply.github.com>
2024-03-05 22:29:36 +0100

    Update and rename 7segemntdisplay_Pinlist to 7segemntdisplay_Pinlist.py

---
+++ Helpfull/7segemntdisplay_Pinlist.py
@@ -0,0 +1,10 @@
+from machine import Pin
+
+middle = machine.Pin((), machine.Pin.OUT)
+topleft = machine.Pin((), machine.Pin.OUT)
+top = machine.Pin((), machine.Pin.OUT)
+topright = machine.Pin((), machine.Pin.OUT)
+dot = machine.Pin((), machine.Pin.OUT)
+bottomright= machine.Pin((), machine.Pin.OUT)
+bottom = machine.Pin((), machine.Pin.OUT)
+bottomleft = machine.Pin((), machine.Pin.OUT)
